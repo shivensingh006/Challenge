ab83e0ef9282ea940d59002528de1bc4ddee7234
shivensingh006 <34125912+shivensingh006@users.noreply.github.com>
2020-03-13 14:30:43 +0530

    Update Challenge.php

--- Challenge.php
+++ Challenge.php
@@ -6,8 +6,8 @@ multiples of 3, instead of the number, print "Linio". For multiples of 5 print
 "IT". For numbers which are multiples of both 3 and 5, print "Linianos".*/
 
 class prints_numbers{
-	public static function get_numbers($array, $range){
-		$number = range(1,$range);
+	public static function get_numbers($array, $start, $end){
+		$number = range($start,$end);
 		$result = array_map(function($num) use ($array){
 			$keys = array_keys($array);
 			foreach($keys as $value){
@@ -24,8 +24,9 @@ class prints_numbers{
 }
 
 $array = array(15=>"Linianos", 3=>"Linio", 5=>"IT");
-$range = 100;
-$result = prints_numbers::get_numbers($array, $range);
+$start = 1;
+$end = 100;
+$result = prints_numbers::get_numbers($array, $start, $end);
 
 
 echo "<pre>";
